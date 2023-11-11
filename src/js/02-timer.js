e23f8b0fefda4ca56518bd98d6d2ccfeacb659b2
serhii_pmt <sergiy.pmt.20@gmail.com>
2023-11-11 11:01:54 +0200

    install flatpickr, import it to 02-timer.js

---
+++ src/js/02-timer.js
@@ -0,0 +1,9 @@
+// add library
+import flatpickr from 'flatpickr';
+
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+// get elemets: input, button, text's spans (days, hours, minutes, seconds)
+// inside input can choose calendar's days, when choose privious days start button block and warning message popup, next days - start button active
+// when push start button inside spans appear time left to the day and time that choosen inside input
