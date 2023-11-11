7148794a60524f97bfdf2da2bd5076a6e6d5699e
serhii_pmt <sergiy.pmt.20@gmail.com>
2023-11-11 10:28:13 +0200

    finish task-1: get buttons and body, add event listeners to buttons that start and stop body color changing

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+//
+// get buttons
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+// get body for changing background
+const body = document.querySelector('body');
+
+// create variable for setInterval (change/stop body bg colog)
+let timerId = null;
+
+// start button activate random color changing with 1 second interval
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  // when push button it disabled until push other button
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+});
+
+stopBtn.addEventListener('click', () => {
+  // stop changing background color
+  clearInterval(timerId);
+
+  // when push button it disabled until push other button
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+});
+
+// generate random colors
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
